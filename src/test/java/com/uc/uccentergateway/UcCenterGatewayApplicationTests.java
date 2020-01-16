e13143bb87495c313070ffb05e321badc4fc37a0
yantaosun <15910344486@163.com>
2020-01-16 16:19:01 +0800

    init

---
+++ src/test/java/com/uc/uccentergateway/UcCenterGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uc.uccentergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcCenterGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
